c1be395bc63741e51d9a018e44c495e74a97c3ac
Jirnyak <50689126+Jirnyak@users.noreply.github.com>
2026-01-20 04:39:16 +0000

    Update battle.h

--- src/battle.h
+++ src/battle.h
@@ -360,7 +360,7 @@ public:
         if (enemy_->type == NPCType::Merchant) s_idx = (size_t)ObjectType::Merchant;
         
         //SDL_RenderCopy(ctx.renderer, textures.sprite(s_idx), nullptr, &enemy_rect);
-        SDL_RenderCopy(ctx.renderer, textures.sprite((size_t)ObjectType::Peasant), nullptr, &enemy_rect); //test
+        SDL_RenderCopy(ctx.renderer, textures.sprite((size_t)ObjectType::Bandit), nullptr, &enemy_rect); //test
 
         // 3. Статы
         draw_bars(ctx, 20, ctx.window_height - 350, p.life, p.max_life, p.will, p.max_will, "Player");
